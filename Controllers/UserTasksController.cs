2fa065ef8c863e7a6622b7ec3a5af5e6bd2808c3
Paulo Castelo <oluap.castelo@gmail.com>
2024-09-07 18:53:50 -0300

    Commit changes.

--- Controllers/UserTasksController.cs
+++ Controllers/UserTasksController.cs
@@ -50,8 +50,6 @@ namespace iDoneWeb.Controllers
         }
 
         // POST: UserTasks/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Id,Title,Description,CreatedDate,DueDate,Status")] UserTask userTask)
@@ -82,8 +80,6 @@ namespace iDoneWeb.Controllers
         }
 
         // POST: UserTasks/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Description,CreatedDate,DueDate,Status")] UserTask userTask)
